a132ae573468fa6a4ae8fba4d32a6a00bf011ae3
Antoine-marchais <antoine.marchais@student.ecp.fr>
2019-11-13 19:35:10 +0100

    Updates

--- precache-manifest.238b2a5186b409f256ea0e60c271bdf4.js
+++ precache-manifest.238b2a5186b409f256ea0e60c271bdf4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "259a5b920ca908387c8b58655894662b",
+    "revision": "4bada955695ce5e614f0559371e19425",
     "url": "/blindtest-spotify/index.html"
   },
   {
-    "revision": "441bb989fb2fa5459b9e",
+    "revision": "bb94f30985b40d4d4e5b",
     "url": "/blindtest-spotify/static/css/main.7c714b3b.chunk.css"
   },
   {
-    "revision": "80c1c6fb04fa11f039ee",
-    "url": "/blindtest-spotify/static/js/2.88605e05.chunk.js"
+    "revision": "5a372fb5324512f82c83",
+    "url": "/blindtest-spotify/static/js/2.5308bb9f.chunk.js"
   },
   {
-    "revision": "441bb989fb2fa5459b9e",
-    "url": "/blindtest-spotify/static/js/main.cd7a001a.chunk.js"
+    "revision": "bb94f30985b40d4d4e5b",
+    "url": "/blindtest-spotify/static/js/main.91d6c7ee.chunk.js"
   },
   {
-    "revision": "d74675ac0a498334cc92",
-    "url": "/blindtest-spotify/static/js/runtime-main.07f395c8.js"
+    "revision": "b2a1665a8eba4ed690bc",
+    "url": "/blindtest-spotify/static/js/runtime-main.8fe5e19e.js"
   },
   {
     "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blindtest-spotify/precache-manifest.af1d10f296fb8bac425bec0e56195ae1.js"
+  "/blindtest-spotify/precache-manifest.238b2a5186b409f256ea0e60c271bdf4.js"
 );
 
 self.addEventListener('message', (event) => {
